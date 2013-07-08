06100d276ec7f95c09218c4a76c1c831803453c0
ChrisToffee <chris.s.dennis@gmail.com>
2013-07-08 14:50:06 +0100

    imporved physics

--- app/js/app.js
+++ app/js/app.js
@@ -90,7 +90,7 @@ jQuery(document).ready ( function () {
     for (i = 0; i <= 120; i++) {
       var cloud = [];
       // Get random positions for stars.
-      var size = Math.floor(Math.random() * 4) + 0.5;
+      var size = Math.floor(Math.random() * 4) + 2.5;
       var speed = (Math.random()*0.00001+0.000001).toFixed(8);
       toggle = !toggle;
 
@@ -117,37 +117,37 @@ jQuery(document).ready ( function () {
   function drawPlanet() {
     context.beginPath();    
     context.fillStyle = "rgba(102, 255, 255, 0.5)";
-    context.arc( canvasWidth / 2, canvasHeight - 10, 385, 0, Math.PI * 5, true );
+    context.arc( canvasWidth / 2, canvasHeight - 10, canvasWidth / 2, 0, Math.PI * 5, true );
     context.closePath();
     context.fill(); 
 
     context.beginPath();    
     context.fillStyle = "rgba(102, 204, 102, 1)";
-    context.arc( canvasWidth / 2, canvasHeight - 10, 335, 0, Math.PI * 5, true );
+    context.arc( canvasWidth / 2, canvasHeight - 10, canvasWidth / 2.2, 0, Math.PI * 5, true );
     context.closePath();
     context.fill(); 
 
     context.fillStyle = 'rgba(51, 25, 0, 1)';
     context.beginPath();
-    context.arc( canvasWidth / 2, canvasHeight - 10, 320, 0, Math.PI * 5, true );
+    context.arc( canvasWidth / 2, canvasHeight - 10, canvasWidth / 2.25, 0, Math.PI * 5, true );
     context.closePath();
     context.fill();
 
     context.fillStyle = 'rgba(102, 51, 0, 1)';
     context.beginPath();
-    context.arc( canvasWidth / 2, canvasHeight - 10, 300, 0, Math.PI * 5, true );
+    context.arc( canvasWidth / 2, canvasHeight - 10, canvasWidth / 3, 0, Math.PI * 5, true );
     context.closePath();
     context.fill(); 
 
     context.fillStyle = 'rgba(255, 178, 102, 1)';
     context.beginPath();
-    context.arc( canvasWidth / 2, canvasHeight - 10, 90, 0, Math.PI * 5, true );
+    context.arc( canvasWidth / 2, canvasHeight - 10, canvasWidth / 12, 0, Math.PI * 5, true );
     context.closePath();
     context.fill(); 
 
     context.fillStyle = 'rgba(255, 225, 102, 1)';
     context.beginPath();
-    context.arc( canvasWidth / 2, canvasHeight - 10, 50, 0, Math.PI * 5, true );
+    context.arc( canvasWidth / 2, canvasHeight - 10, canvasWidth / 18, 0, Math.PI * 5, true );
     context.closePath();
     context.fill(); 
   }
@@ -164,8 +164,8 @@ jQuery(document).ready ( function () {
         	time = -time;
         }
 
-        var cloudx = Math.sin( time ) * (370 + cloudSize *3) + canvasWidth / 2;
-        var cloudy = Math.cos( time ) * (370 + cloudSize *3) + canvasHeight;
+        var cloudx = Math.sin( time ) * ((canvasWidth / 2.12) + cloudSize *4) + canvasWidth / 2;
+        var cloudy = Math.cos( time ) * ((canvasWidth / 2.12) + cloudSize *4) + canvasHeight;
 
         var cloud2x = Math.sin( time - 2 ) * 5 + cloudx;
         var cloud2y = Math.cos( time - 2 ) * 5 + cloudy;
@@ -181,14 +181,14 @@ jQuery(document).ready ( function () {
         context.fill();
 
         context.beginPath();
-        context.fillStyle = "rgba(255, 255, 255, 0.2)";
+        context.fillStyle = "rgba(255, 255, 255, 0.4)";
         context.arc( cloud2x, cloud2y, cloudSize / 2, 0, Math.PI * 5, true );
         context.closePath();
 
         context.fill();
 
         context.beginPath();
-        context.fillStyle = "rgba(255, 255, 255, 0.2)";
+        context.fillStyle = "rgba(255, 255, 255, 0.4)";
         context.arc( cloud3x, cloud3y, cloudSize /1.5, 0, Math.PI * 5, true );
         context.closePath();
 
@@ -199,8 +199,8 @@ jQuery(document).ready ( function () {
 
   function drawResources () {
 
-    var x = ((Math.sin( planetPosition ) * (200)) + canvasWidth / 2) ;
-    var y = ((Math.cos( planetPosition ) * (200)) + canvasHeight);
+    var x = ((Math.sin( planetPosition ) * (canvasWidth / 8)) + canvasWidth / 2) ;
+    var y = ((Math.cos( planetPosition ) * (canvasWidth / 8)) + canvasHeight);
 
     //gold
     context.beginPath();
@@ -223,8 +223,8 @@ jQuery(document).ready ( function () {
     context.fillRect(x -6,y - 6,5,5);
     context.fill();
 
-    x = ((Math.sin( planetPosition ) * (270)) + canvasWidth / 2) ;
-    y = ((Math.cos( planetPosition ) * (270)) + canvasHeight);
+    x = ((Math.sin( planetPosition +0.5 ) * (canvasWidth / 4)) + canvasWidth / 2) ;
+    y = ((Math.cos( planetPosition +0.5) * (canvasWidth / 4)) + canvasHeight);
     //coal
     context.beginPath();
     context.fillStyle="black";
@@ -245,20 +245,52 @@ jQuery(document).ready ( function () {
     context.fillStyle="black";
     context.fillRect(x -6,y - 6,5,5);
     context.fill();
+
+    x = ((Math.sin( planetPosition -1.5 ) * (canvasWidth / 6)) + canvasWidth / 2) ;
+    y = ((Math.cos( planetPosition -1.5) * (canvasWidth / 6)) + canvasHeight);
+
+    //jewles
+    context.beginPath();
+    context.fillStyle="white";
+    context.fillRect(x,y - 5,5,5);
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle="aqua";
+    context.fillRect(x + 5,y ,3,3);
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle="aqua";
+    context.fillRect(x - 15,y - 8,2,2);
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle="white";
+    context.fillRect(x -6,y - 6,5,5);
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle="white";
+    context.fillRect(x -1,y + 10,5,5);
+    context.fill();
   }
 
   function drawFactory () {
 
-    var y = (Math.sin( planetPosition ) * (360)) + canvasWidth / 2 - 18;
-    var x = (Math.cos( planetPosition ) * (360)) + canvasHeight ;
+    var y = (Math.sin( planetPosition ) * (canvasWidth / 2.15)) + canvasWidth / 2;
+    var x = (Math.cos( planetPosition ) * (canvasWidth / 2.15)) + canvasHeight ;
     var r = Math.PI / planetPosition;
-    context.save();
+    
     
 
     context.beginPath();
     
     context.fillStyle="#FF0000";
-    
+    context.fill();
+    context.save();
+    context.rotate(r);
+    context.restore();
     context.fillRect(y,x,5,18);
 
     context.fill();
@@ -266,9 +298,7 @@ jQuery(document).ready ( function () {
     context.beginPath();
     context.fillStyle="#FF0000";
     context.fillRect(y,x +5,50,15);
-    context.fill();
-    context.rotate(r);
-    context.restore();
+    
 
   }
 
